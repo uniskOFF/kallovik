ad514c06d73f56d0acb54394d317ea877930bdf6
uniskOFF <uniskOFF@users.noreply.github.com>
2026-07-31 19:17:38 +0000

    deploy: 01d3114622068625b0e3f72eaf42f073bbd668aa

--- _next/static/kwottdV8A0AXb0vTTWyfe/_buildManifest.js
+++ _next/static/kwottdV8A0AXb0vTTWyfe/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/kallovik//_next/:path+",
+        "destination": "/kallovik/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
